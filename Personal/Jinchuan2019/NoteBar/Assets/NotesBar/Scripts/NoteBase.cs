0c9b2f987248823437a6ad9bf72b42356efed2e7
Jinchuan2019 <50429790+Jinchuan2019@users.noreply.github.com>
2019-05-24 01:00:30 +0900

    ノートの表示(未完成)
    ノートの表示(未完成)

---
+++ Personal/Jinchuan2019/NoteBar/Assets/NotesBar/Scripts/NoteBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class NoteBase : MonoBehaviour
+{
+    public abstract void SetNextDuration(int duration);
+    public abstract void DoNextNote();
+    public abstract void SetId(int id);
+    protected int duration;
+    protected int id;
+    protected int maxId;
+    //画面上ノーツの距離
+    [SerializeField]
+    protected float intervalSize;
+}
---
+++ Personal/Jinchuan2019/NoteBar/Assets/NotesBar/Scripts/NotesManager.cs
@@ -0,0 +1,25 @@
+﻿//======================================//
+/*******作成者:金川　2019-05-23**********/
+//======================================//
+/*ノーツの表示 */
+//======================================//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotesManager : MonoBehaviour
+{
+    //ノーツの移動の向き
+    [SerializeField]
+    bool fromRightToLeft;
+    //次の判定タイミング来るまでの秒数
+    [SerializeField]
+    float nextDuration;
+    //ノートの個数
+    [SerializeField]
+    int totalNotes;
+    //表示させたいノートのプリハブ
+    [SerializeField]
+    GameObject notePrefab;
+}
