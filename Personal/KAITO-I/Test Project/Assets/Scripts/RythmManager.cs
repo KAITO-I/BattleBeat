b62bf64896d9075aba1a7325aa89c304ff9aecfa
KAITO-I <inafuku.tnw@gmail.com>
2019-05-27 16:14:25 +0900

    リズム処理のブラッシュアップ中

---
+++ Personal/KAITO-I/Test Project/Assets/Scripts/RythmManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//==============================
+// リズム管理
+//==============================
+public class RythmManager : MonoBehaviour
+{
+    // 設定
+    [SerializeField] private float bpm = 100f;
+    public float BPM { get { return this.bpm; } }
+
+    // 測定
+    private float bps;
+    private float time;
+
+    // 命令先
+    [SerializeField] private UnityEvent events = new UnityEvent();
+
+    private void Start()
+    {
+        this.bps = 60f / bpm;
+        this.time = 0f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void Execute()
+    {
+
+    }
+
+    private void InvokeEvent()
+    {
+        events.Invoke();
+    }
+}
