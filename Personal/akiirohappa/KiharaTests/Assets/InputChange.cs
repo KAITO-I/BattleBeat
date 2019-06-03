4d7363faebd190669f8016fb09bf22b7c6bd3a2a
akiirohappa <kihara.k2000@gmail.com>
2019-06-03 14:29:22 +0900

    コントローラー関連

--- Personal/akiirohappa/KiharaTests/Assets/InputChange.cs
+++ Personal/akiirohappa/KiharaTests/Assets/InputChange.cs
@@ -6,7 +6,7 @@ public class InputChange : MonoBehaviour
 {
     public Text uitex;
     PlayerNum pn;
-    public PlayerData[] players; 
+    public PlayerController[] players; 
     // Start is called before the first frame update
     void Start()
     {
