322fc520f7670f0fe8ea0399504c2fc0c04f075c
KAITO-I <inafuku.tnw@gmail.com>
2019-06-03 15:56:56 +0900

    マスターデータにシーンを追加（α用）、個人データの編集

---
+++ Personal/KAITO-I/Test Project/Assets/Scripts/Title/ConfigController.cs
@@ -0,0 +1,17 @@
+﻿//==============================
+// Created by KAITO-I.
+//==============================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==============================
+// コンフィグ操作
+//==============================
+public class ConfigController : MonoBehaviour
+{
+    public void Updated()
+    {
+
+    }
+}
---
+++ Personal/KAITO-I/Test Project/Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,35 @@
+﻿//==============================
+// Created by KAITO-I.
+//==============================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TitleState
+{
+    MainMenu, // モード選択画面
+    Config,   // コンフィグ
+    Credit    // クレジット
+}
+
+//==============================
+// タイトル管理
+//==============================
+public class TitleManager : MonoBehaviour
+{
+    private TitleState state;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case TitleState.MainMenu:
+
+        }
+    }
+}
