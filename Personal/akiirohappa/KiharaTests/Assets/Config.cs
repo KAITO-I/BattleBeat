97a6b4f72268f696063658514e3f1d92096cd48b
akiirohappa <kihara.k2000@gmail.com>
2019-06-17 15:41:52 +0900

    コンフィグ

---
+++ Personal/akiirohappa/KiharaTests/Assets/Config.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Config : MonoBehaviour
+{
+    [SerializeField] float BGM;
+    [SerializeField] float SE;
+    [SerializeField] Slider[] Sliders;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM = LoadVol("BGM");
+        SE = LoadVol("SE");
+        Sliders[0].value = BGM;
+        Sliders[1].value = SE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBGMVol(float f)
+    {
+        BGM = f;
+        SaveVol(BGM, "BGM");
+    }
+    public void SetSEVol(float f)
+    {
+        SE = f;
+        SaveVol(SE, "SE");
+    }
+    public void SaveVol(float val,string key)
+    {
+        PlayerPrefs.SetFloat(key,val);
+        PlayerPrefs.Save();
+    }
+    public float LoadVol(string key)
+    {
+        return PlayerPrefs.GetFloat(key, 0.5f);
+    }
+}
