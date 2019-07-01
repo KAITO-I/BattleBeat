033c0917405eb379b805ec6a8b675f4f3af80e8d
kazu-ki11 <kazuki.nh11@gmail.com>
2019-07-01 16:19:13 +0900

    個人データの更新

---
+++ Personal/kazu-ki11/BBtest3/BBtest3/BBtest/Assets/SelectButtom.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SelectButtom : MonoBehaviour
+{
+    //選択取っているので入力はできるよ
+
+    //ボタン認識->リスト化？構造化？
+    //最初
+    [SerializeField]Button PlayMode; [SerializeField] Button Cfg; [SerializeField] Button Credit; [SerializeField] Button Back;
+    //二個目
+    [SerializeField] Button PlayGame; [SerializeField] Button Training; [SerializeField] Button BackSecond;
+    //キャンバス内グループオブジェクト
+    GameObject Grup2;
+
+    //最初の選択
+    //二番目の選択へ
+    public void OnClickPlayMode()
+    {
+        //カーソル指定とローテーション・背景操作
+        Debug.Log("mode選択へ");
+        PlayGame.Select();
+    }
+//設定移動
+    public void OnClickCfg()
+    {
+        Debug.Log("Config");
+    }
+//クレジット・スタッフロール
+    public void OnClickCredit()
+    {
+        Debug.Log("Credit");
+    }
+//タイトルへ戻る
+    public void OnClickBack()
+    {
+        Debug.Log("GoMainTitle");
+    }
+
+    //二番目の選択
+    //ゲーム開始ボタン
+    public void OnClickPlayGame()
+    {
+        Debug.Log("Play Game!");
+        SceneManager.LoadScene("PlayGame");
+    }
+    //練習・訓練ボタン
+    public void OnClickTraining()
+    {
+        //Debug.Log("GoTrainig");
+        SceneManager.LoadScene("PlayGame");
+    }
+    //最初の選択に戻る
+    public void OnCkickBackSecond()
+    {
+        //カーソル指定とローテーション・背景操作
+        Debug.Log("最初の選択へ");
+        PlayMode.Select();
+    }
+
+    private void Start()
+    {
+        //最初のボタンをセット
+        PlayMode = GameObject.Find("/Canvas/PlayMode").GetComponent<Button>();
+        Cfg = GameObject.Find("/Canvas/Config").GetComponent<Button>();
+        Credit = GameObject.Find("/Canvas/Credit").GetComponent<Button>();
+        Back = GameObject.Find("/Canvas/Back(MainTitle)").GetComponent<Button>();
+        //二つ目
+        PlayGame = GameObject.Find("/Canvas/PlaySelect/Battle").GetComponent<Button>();
+        Training = GameObject.Find("/Canvas/PlaySelect/Training").GetComponent<Button>();
+        BackSecond = GameObject.Find("/Canvas/PlaySelect/Back(Select)").GetComponent<Button>();
+        //ボタン初期選択
+        PlayMode.Select();
+
+        //グループをセット
+        Grup2 = GameObject.Find("Canvas/PlaySelect").GetComponent<GameObject>();
+    }
+
+}
---
+++ Personal/kazu-ki11/BBtest3/BBtest3/BBtest/Assets/tamesiSelectButtom.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tamesiSelectButtom : MonoBehaviour
+{
+    private string LoadName;
+
+    public void ClickPlayGame(int i)
+    {
+        switch (i)
+        {
+            case 1://ゲーム開始ボタン
+                LoadName = "PlayGame";
+                break;
+
+            case 2://練習・訓練ボタン
+                LoadName = "Training";
+                break;
+
+            case 3://設定移動
+                LoadName = "Config";
+                break;
+
+            case 4://クレジット・スタッフロール
+                LoadName = "Credit";
+                break;
+
+            case 5://タイトルへ戻る
+                LoadName = "Title";
+                break;
+
+            default:
+                break;
+        }
+    
+    }
+    private void Update()
+    {
+        if(LoadName != null)//シーン外を指定するとエラーもしくは存在しない場合
+        {
+            Debug.Log(LoadName);
+            SceneManager.LoadScene(LoadName);
+        }
+    }
+}
