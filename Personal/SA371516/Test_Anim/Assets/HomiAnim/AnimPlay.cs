85a26fde1ea8f6f0118d61f4d944ecdeb57e37ab
SA371516 <baseball.syunta@gmail.com>
2019-07-02 19:59:22 +0900

    キャラクターのアニメーションの実装プロジェクト

---
+++ Personal/SA371516/Test_Anim/Assets/HomiAnim/AnimPlay.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPlay : MonoBehaviour
+{
+    Animator anim;
+    AnimatorStateInfo info_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        info_ = anim.GetCurrentAnimatorStateInfo(0);
+        //連打処理未対応
+        //アニメーションがidolに代わる前に位置をずらすとちょうどいい
+        if (info_.IsName("idol"))
+        {
+            TestAnim();
+        }
+    }
+    void TestAnim()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("LeftT");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("RightT");
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("BackT");
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("FlontT");
+        }
+    }
+}
