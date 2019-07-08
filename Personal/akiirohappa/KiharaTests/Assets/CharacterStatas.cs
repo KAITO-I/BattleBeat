ae4ec4b06b4e5e5eb21bc6605b650295df997a6b
akiirohappa <kihara.k2000@gmail.com>
2019-07-08 15:20:57 +0900

    メインでのキャラの生成

--- Personal/akiirohappa/KiharaTests/Assets/CharacterStatas.cs
+++ Personal/akiirohappa/KiharaTests/Assets/CharacterStatas.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
+﻿//------------------------------------
+//作成者：木原　キャラクターステータス
+//------------------------------------
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName ="Chara",fileName ="Character")]
-public class CharacterStasats : ScriptableObject
+[CreateAssetMenu(fileName ="Character",menuName ="Chara")]
+public class CharacterStatas : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int id = 0;
+    public int HP = 20;
+    //攻撃方法・スキル
+    public GameObject Skill1;
+    public GameObject Skill2;
+    public GameObject Skill3;
+    public GameObject Skill4;
+    public GameObject Charaprefab;
 }
--- Personal/akiirohappa/KiharaTests/Assets/PlayerStatas.cs
+++ Personal/akiirohappa/KiharaTests/Assets/PlayerStatas.cs
@@ -4,49 +4,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+struct PlData
+{
+    public int id;//キャラID
+    public int hp;//現在HP
+    public int sp;//現在SP
+    public CharacterStatas mychara;
+}
 public class PlayerStatas : MonoBehaviour
 {
     [SerializeField] int pnum;//プレイヤー番号
-    [SerializeField] int id;//キャラID
-    [SerializeField] int hp;//現在HP
-    [SerializeField] int sp;//現在SP
-
+    private static PlData[] playerDatas = new PlData[2];
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init();
+        playerDatas[0].id = 1;
+        playerDatas[1].id = 3;
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
-    public int GetId()
+    public void Init()
     {
-        return id;
+        for(int i = 0;i < playerDatas.Length; i++)
+        {
+            playerDatas[i].id = 0;
+            playerDatas[i].hp = 20;
+            playerDatas[i].sp = 0;
+            playerDatas[i].mychara = null;
+        }
+       
     }
-    public void SetId(int i)
+
+    public int GetId(int num)
     {
-        id = i;
+        return playerDatas[num-1].id;
     }
-    public int GetHP()
+    public void SetId(int num,int i)
     {
-        return hp;
+        playerDatas[num - 1].id = i;
+    }
+    public int GetHP(int num)
+    {
+        return playerDatas[num - 1].hp;
     }
     //HP減少(回復するならvalueを-にして)
-    public void SetHP(int value)
+    public void SetHP(int num,int value)
     {
-        hp -= value;
+        playerDatas[num - 1].hp -= value;
     }
-    public int GetSP()
+    public int GetSP(int num)
     {
-        return sp;
+        return playerDatas[num - 1].sp;
     }
-    public void SetSP(int value)
+    public void SetSP(int num,int value)
     {
-        sp += value;
+        playerDatas[num - 1].sp += value;
+    }
+    public CharacterStatas GetChara(int num)
+    {
+        return playerDatas[num - 1].mychara;
+    }
+    public void SetChara(int num,CharacterStatas chara)
+    {
+        playerDatas[num - 1].mychara = chara;
     }
 }
