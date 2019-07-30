693b2f4862d67cabb70954cdd6b2e462dce3ac1f
KAITO-I <inafuku.tnw@gmail.com>
2019-07-30 23:45:18 +0900

    ロード時の時間（デバッグ用）を短くしました

--- Battle Beat - Alpha/Assets/Scripts/DontDestroyOnLoad/SceneLoader/SceneLoader.cs
+++ Battle Beat - Alpha/Assets/Scripts/DontDestroyOnLoad/SceneLoader/SceneLoader.cs
@@ -157,7 +157,7 @@ public class SceneLoader : MonoBehaviour
         // 修正
         this.loadingGauge.fillAmount = 1f;
 
-        yield return new WaitForSeconds(10.0f);
+        yield return new WaitForSeconds(3.0f);
 
         // ローディング文字コルーチン停止
         StopCoroutine(loadingTextAnim);
