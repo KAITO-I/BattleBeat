b1c163688ee7248c2cf5d7bd6ee5f43277b6ad69
S_A1516 <baseball.syunta@gmail.com>
2019-09-02 00:25:25 +0900

    リザルト画面実装 キャラの透明度とセリフの違いをまだ未実装

--- Battle Beat - Alpha/Assets/Scripts/ScenesManager/ResultManager.cs
+++ Battle Beat - Alpha/Assets/Scripts/ScenesManager/ResultManager.cs
@@ -18,15 +18,42 @@ public class ResultManager : MonoBehaviour
         public Sprite _backImg;
         public Sprite _charaImg;
         public Sprite _CharaText;
-        public string _Word;
+        public string _disWord;
     }
     //キャラクター台詞
-    string[] Words ={
-        "カグラ勝利,これはテストの表示です",
-        "ホーミー勝利,これはテストの表示です",
-        "アナ勝利,これはテストの表示です",
-        "ユニ＆ゾーン勝利,これはテストの表示です "
+    string[,] _words =
+    {  
+        {
+            "悪くなかった。\nいずれまた、手合わせを願おう。",
+            "もう少しマシなものは居ないのか？\n……これでは腕が鈍るばかりだ。",
+            "六弦琴を振るい戦うか\n……小僧、中々に楽しめたぞ。",
+            "その腕の武装\n……少し見せてはもらえないだろうか？",
+            "なんとも珍妙なものに乗っているな。\n戦の流儀も人それぞれ、か。"
+        },
+
+        {
+            "楽しかったぜ！またやろうな！！",
+            "今日も良いビートだった！アンタも結構ノれてたぜ？",
+            "そのガッチガチな装備でよく動けんな……重くねーの？",
+            "オネーサンの店にライブステージ作ってくれよ！毎日盛り上げてやっからさ！	",
+            "お前らのゴーグルお揃いなのか？ッハハ、仲いいんだな！"
+        },
+        {
+            "",
+            "",
+            "",
+            "",
+            ""
+        },
+        {
+            "",
+            "",
+            "",
+            "",
+            ""
+        },
     };
+
     //ここのintをSettingのCharaに変えると全て変わる
     Dictionary<int, CharaStatus> chara = new Dictionary<int, CharaStatus>();
     ResultState state;
@@ -38,16 +65,16 @@ public class ResultManager : MonoBehaviour
     private float timeElapsed = 1;          // 文字列の表示を開始した時間
     private int lastUpdateCharacter = -1;       // 表示中の文字数
     int flag;
+    Color changecolor = new Color();
 
-    Color changecolor;
-    [SerializeField]
-    GameObject[] Moves;
     [SerializeField]
     Image BackGraund, CharaImg;
     [SerializeField]
     GameObject BlackImg;
+    [SerializeField,Header("動かす画像")]
+    GameObject[] Moves;
     //背景の画像
-    [SerializeField]
+    [SerializeField,Header("オブジェクトリスト")]
     Sprite[] BackImgs, CharaImags, PlayerImgs, CharaTexts;
     [SerializeField]
     GameObject WordPos;
@@ -57,12 +84,14 @@ public class ResultManager : MonoBehaviour
     [SerializeField]
     int WinCharaID;
     [SerializeField]
+    float _loseCharaId;
+    [SerializeField]
     [Range(0.001f, 0.3f)]
     float intervalForCharacterDisplay = 0.05f;  // 1文字の表示にかかる時間
     public float MoveTime;
+    float _yPos = -91;
 
-    float _yPos = -105;
-
+    //キャラの位置調節に必要
     float[] _xSize =
     {
         1.2f,
@@ -77,15 +106,17 @@ public class ResultManager : MonoBehaviour
         1.2f,
         1f
     };
-
-
+    
     void Start()
     {
+        BlackImg.SetActive(true);
+
         //キャラ情報初期化
         for(int i = 0; i < 4; ++i)
         {
             CharaStatus status = new CharaStatus();
-            status._Word = Words[i];
+            //セリフをランダムに出現させるため
+            status._disWord = CharaWoadInstance();
             status._backImg = BackImgs[i];
             status._charaImg = CharaImags[i];
             status._CharaText = CharaTexts[i];
@@ -104,17 +135,22 @@ public class ResultManager : MonoBehaviour
             pos.x = -300;
             Moves[i].gameObject.transform.position = pos;
         }
-        //立ち絵位置修正
+        //アナのみ立ち絵位置修正
         RectTransform rect = CharaImg.GetComponent<RectTransform>();
         if (WinCharaID == 3)
         {
             Vector3 _vec = rect.position;
-            _vec.y = _yPos;
+            _vec.y += _yPos;
             CharaImg.GetComponent<RectTransform>().position = _vec;
         }
         rect.transform.localScale = new Vector3(_xSize[WinCharaID], _ySize[WinCharaID], 1f);
         //背景
         BackGraund.sprite = chara[WinCharaID - 1]._backImg;
+        ////色初期化
+        changecolor = Color.white;
+        //changecolor.a = 0.1f;
+        //CharaImg.color = changecolor;
+
         //キャラクター
         CharaImg.sprite = chara[WinCharaID - 1]._charaImg;
         charatext.sprite = chara[WinCharaID - 1]._CharaText;
@@ -123,12 +159,6 @@ public class ResultManager : MonoBehaviour
 
         WordPos.SetActive(false);
         state = ResultState.BackDis;
-
-        //色初期化
-        changecolor = new Color();
-        changecolor = Color.white;
-        changecolor.a = 0f;
-        CharaImg.color = changecolor;
     }
 
     // Update is called once per frame
@@ -146,12 +176,17 @@ public class ResultManager : MonoBehaviour
                 //背景の透明度を変えている
             case ResultState.CharaDis:
                 changecolor.a += 0.1f;
-                CharaImg.color = changecolor;
-                if (CharaImg.color.a >= 1)
+                if (changecolor.a >= 1)
                 {
-                    SetText();
+                    changecolor.a = 1f;
+                    CharaImg.color = changecolor;
+                    TextInstance();
                     WordPos.SetActive(true);
                     state = ResultState.TextDis;
+                }
+                else
+                {
+                    CharaImg.color = changecolor;
                 }
                 break;
             case ResultState.TextDis:
@@ -179,35 +214,154 @@ public class ResultManager : MonoBehaviour
     void TextDis()
     {
         // クリックから経過した時間が想定表示時間の何%か確認し、表示文字数を出す
-        int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * chara[WinCharaID - 1]._Word.Length);
+        int displayCharacterCount = (int)(Mathf.Clamp01((Time.time - timeElapsed) / timeUntilDisplay) * chara[WinCharaID - 1]._disWord.Length);
         // 表示文字数が前回の表示文字数と異なるならテキストを更新する
         if (displayCharacterCount != lastUpdateCharacter)
         {
-            Word.text = chara[WinCharaID-1]._Word.Substring(0, displayCharacterCount);
+            Word.text = chara[WinCharaID-1]._disWord.Substring(0, displayCharacterCount);
             lastUpdateCharacter = displayCharacterCount;
         }
         //すべて表示したら飛べるようにする
-        if (displayCharacterCount == chara[WinCharaID - 1]._Word.Length)
+        if (displayCharacterCount == chara[WinCharaID - 1]._disWord.Length)
         {
             state = ResultState.SceneJump;
         }
 
     }
+
+    string CharaWoadInstance()
+    {
+        string _charaText = null;
+        //int _charaWordId = Random.Range(0, 3);
+        int _charaWordId = Random.Range(0, 2);
+        switch (WinCharaID)
+        {
+            case 1:
+                if (_charaWordId==2)
+                {
+                    switch (_loseCharaId)
+                    {
+                        case 2:
+                            _charaText = _words[WinCharaID - 1, 2];
+                            break;
+                        case 3:
+                            _charaText = _words[WinCharaID - 1, 3];
+                            break;
+                        case 4:
+                            _charaText = _words[WinCharaID - 1, 4];
+                            break;
+                        default:
+                            _charaWordId = Random.Range(0, 2);
+                            _charaText = _words[WinCharaID - 1, _charaWordId];
+                            break;
+                    }
+
+                }
+                else
+                {
+                    _charaText = _words[WinCharaID - 1, _charaWordId];
+                }
+                break;
+            case 2:
+                if (_charaWordId == 2)
+                {
+                    switch (_loseCharaId)
+                    {
+                        case 1:
+                            _charaText = _words[WinCharaID - 1, 2];
+                            break;
+                        case 3:
+                            _charaText = _words[WinCharaID - 1, 3];
+                            break;
+                        case 4:
+                            _charaText = _words[WinCharaID - 1, 4];
+                            break;
+                        default:
+                            _charaWordId = Random.Range(0, 2);
+                            _charaText = _words[WinCharaID - 1, _charaWordId];
+                            break;
+
+                    }
+
+                }
+                else
+                {
+                    _charaText = _words[WinCharaID - 1, _charaWordId];
+                }
+
+                break;
+            case 3:
+                if (_charaWordId == 2)
+                {
+                    switch (_loseCharaId)
+                    {
+                        case 1:
+                            _charaText = _words[WinCharaID - 1, 2];
+                            break;
+                        case 2:
+                            _charaText = _words[WinCharaID - 1, 3];
+                            break;
+                        case 4:
+                            _charaText = _words[WinCharaID - 1, 4];
+                            break;
+                        default:
+                            _charaWordId = Random.Range(0, 2);
+                            _charaText = _words[WinCharaID - 1, _charaWordId];
+                            break;
+                    }
+                }
+                else
+                {
+                    _charaText = _words[WinCharaID - 1, _charaWordId];
+                }
+
+                break;
+            case 4:
+                if (_charaWordId == 2)
+                {
+                    switch (_loseCharaId)
+                    {
+                        case 1:
+                            _charaText = _words[WinCharaID - 1, 2];
+                            break;
+                        case 3:
+                            _charaText = _words[WinCharaID - 1, 3];
+                            break;
+                        case 4:
+                            _charaText = _words[WinCharaID - 1, 4];
+                            break;
+                        default:
+                            _charaWordId = Random.Range(0, 2);
+                            _charaText = _words[WinCharaID - 1, _charaWordId];
+                            break;
+                    }
+                }
+                else
+                {
+                    _charaText = _words[WinCharaID - 1, _charaWordId];
+                }
+                break;
+            default:
+                _charaText = "範囲外が選択されました";
+                break;
+        }
+        return _charaText;
+    }
+
     //テキスト表示初期化
-    void SetText()
+    void TextInstance()
     {
         // 想定表示時間と現在の時刻をキャッシュ
-        timeUntilDisplay = chara[WinCharaID - 1]._Word.Length * intervalForCharacterDisplay;
+        timeUntilDisplay = chara[WinCharaID - 1]._disWord.Length * intervalForCharacterDisplay;
         timeElapsed = Time.time;
         // 文字カウントを初期化
         lastUpdateCharacter = -1;
     }
 
-
-    //
     void TextMove()
     {
         RectTransform rect = Moves[flag].GetComponent<RectTransform>();
+        //動かす順番を変えている
         switch (flag)
         {
             case 0:
