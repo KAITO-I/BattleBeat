3893d01617f8a823f1f02e5d4bba86a3b3a5aa6a
KAITO-I <inafuku.tnw@gmail.com>
2019-11-01 10:09:49 +0900

    Prefabとスクリプト仮作成

---
+++ Battle Beat - Master/Assets/Scripts/DontDestroyOnLoad/ModeDisplayer/ModeDisplayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CoreManager {
+    class ModeDisplayer
+    {
+        static ModeDisplayer instance;
+        public static ModeDisplayer Instance
+        {
+            get
+            {
+                if (instance == null) Debug.LogError("ModeDisplayerがインスタンスされていません。");
+                return instance;
+            }
+        }
+
+        GameObject go;
+        Image      image;
+        Text       text;
+
+        internal ModeDisplayer(GameObject modeDisplayer)
+        {
+            if (ModeDisplayer.instance != null) return;
+            ModeDisplayer.instance = this;
+
+            this.go    = modeDisplayer;
+            this.image = modeDisplayer.transform.Find("Image").GetComponent<Image>();
+            this.text  = this.image.transform.Find("Text").GetComponent<Text>();
+
+            SetActive(false);
+        }
+
+        public void SetActive(bool active)
+        {
+            this.go.SetActive(active);
+        }
+
+        public void SetImage(Sprite sprite)
+        {
+            this.image.sprite = sprite;
+        }
+
+        public void SetText(string text)
+        {
+            this.text.text = text;
+        }
+    }
+}
