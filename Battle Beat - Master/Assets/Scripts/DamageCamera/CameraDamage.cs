b1156990c6f6bffa9987bee1a321f0aebab05e0e
company <company@company.com>
2020-01-20 11:38:46 +0900

    ShakeCameraの作成(揺れの処理を行う) CameraDamageを呼び出しのみの処理に

---
+++ Battle Beat - Master/Assets/Scripts/DamageCamera/CameraDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDamage : MonoBehaviour
+{
+    [SerializeField]
+    private ShakeCamera shake = null;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            shake.Shake();
+        }
+    }
+}
---
+++ Battle Beat - Master/Assets/Scripts/DamageCamera/ShakeCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShakeCamera : MonoBehaviour
+{
+    private const float shakeTime = 0.2f;
+    private const float shakeWidth = 0.15f;
+
+    /// <summary>
+    /// 外部呼出しメソッド
+    /// </summary>
+    public void Shake()
+    {
+        StartCoroutine(ShakeCoroutine());
+    }
+
+    /// <summary>
+    /// 揺れ処理を行うコルーチン
+    /// </summary>
+    private IEnumerator ShakeCoroutine()
+    {
+        float measure = 0; //時間計測用
+        Vector3 defaultCameraPos = Camera.main.transform.position; //元々のカメラ位置
+
+        while (measure < shakeTime)
+        {
+            Vector3 nowCameraPos = Camera.main.transform.position;
+
+            //揺れ
+            float y = Random.Range(-shakeWidth, shakeWidth);
+            nowCameraPos.y = defaultCameraPos.y + y;
+            Camera.main.transform.position = nowCameraPos;
+            
+            measure += Time.deltaTime;
+            yield return null;
+        }
+        //元の位置に戻す
+        Camera.main.transform.position = defaultCameraPos;
+    }
+}
