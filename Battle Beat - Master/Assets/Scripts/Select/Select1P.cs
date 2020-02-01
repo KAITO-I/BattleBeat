78ca150e6a4b7cab8dc4bfcf5708eb58f7592cd7
S_A1516 <baseball.syunta@gmail.com>
2020-02-01 22:56:09 +0900

    セレクト画面＆ユニゾントラップ完成

--- Battle Beat - Master/Assets/Scripts/Select/Select1P.cs
+++ Battle Beat - Master/Assets/Scripts/Select/Select1P.cs
@@ -11,6 +11,7 @@ public class Select1P : BaseSelect
         _controll.CharaObj[_charactorID].charaSelect(1, true);//選択画面の枠を変える処理
         Gole = Teap.transform.position;
         Gole2 = Teap.transform.position + new Vector3(-500f, 0, 0);
+        _teapMoveTime = ReadyBerMove(_playerOK, _teapMoveTime);
     }
 
     public override void playerUpdate()
@@ -18,9 +19,9 @@ public class Select1P : BaseSelect
         InputProcess(1);
         ButtonInput();
         _teapMoveTime = ReadyBerMove(_playerOK, _teapMoveTime);
-        _playerDescrition.sprite = discritions[_charactorID]._discritionSprites[_charactorDecritionID];
-        _playerDescrition.enabled = _playerDecritionOK;
-        _playerPicture.sprite = _controll.CharaObj[_charactorID].GetCharaSprite;
+        _charactorDescrition.sprite = discritions[_charactorID]._discritionSprites[_charactorDecritionID];
+        _charactorDescrition.enabled = _playerDecritionOK;
+        _charactorPicture.sprite = _controll.CharaObj[_charactorID].GetCharaSprite;
         _playerNameImg.sprite = _charaName[_charactorID];
     }
 
